6a5a92ff541c3dd037a8c95635483144ec9d3f57
Neil Everette <neil.everette@gmail.com>
2024-07-13 17:12:35 -0500

    js to pass values from form to code.ts file

--- code.ts
+++ code.ts
@@ -1,3 +1,11 @@
 figma.showUI(__html__);
 
 figma.ui.resize(500, 500);
+
+figma.ui.onmessage = pluginMessage => {
+    console.log(pluginMessage.username);
+    console.log(pluginMessage.name);
+    console.log(pluginMessage.description);
+    console.log(pluginMessage.darkModeState);
+    console.log(pluginMessage.imageVariant);
+};
