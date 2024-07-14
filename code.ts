653135e86a6f94470d7d4fd7974f7ea0b7906683
Neil Everette <neil.everette@gmail.com>
2024-07-14 18:19:09 -0500

    created a new feature branch

--- code.ts
+++ code.ts
@@ -7,9 +7,12 @@ figma.loadAllPagesAsync();
 figma.ui.onmessage = pluginMessage => {
         
     const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
-    
     const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
+    const defaultDark = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "Image=none, Dark mode=true") as ComponentNode;
+    
+    
     defaultVariant.createInstance();
+    defaultDark.createInstance();
 
     console.log(postComponentSet);
     console.log(postComponentSet.children);
