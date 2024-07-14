f1da3527c69c9848ca1ba7c0932444351edf1d94
Neil Everette <neil.everette@gmail.com>
2024-07-14 09:08:32 -0500

    Added the findOne method

--- code.ts
+++ code.ts
@@ -2,9 +2,18 @@ figma.showUI(__html__);
 
 figma.ui.resize(500, 500);
 
+
 figma.ui.onmessage = pluginMessage => {
-    console.log(pluginMessage.username);
+    figma.loadAllPagesAsync();
+    const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
+    
+    console.log(postComponentSet);
+    console.log(postComponentSet.children);
+    console.log(postComponentSet.name);
+
+
     console.log(pluginMessage.name);
+    console.log(pluginMessage.username);
     console.log(pluginMessage.description);
     console.log(pluginMessage.darkModeState);
     console.log(pluginMessage.imageVariant);
