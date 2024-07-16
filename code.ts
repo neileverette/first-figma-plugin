855ba40bfba0cde21ea1f0f9db1bcfedd54baa91
Neil Everette <neil.everette@gmail.com>
2024-07-15 21:44:44 -0500

    Added if statent to toggle dark mode

--- code.ts
+++ code.ts
@@ -8,19 +8,12 @@ figma.ui.onmessage = pluginMessage => {
         
     const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
     const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
-    const defaultDark = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "Image=none, Dark mode=true") as ComponentNode;
-    
-    
-    defaultVariant.createInstance();
-    defaultDark.createInstance();
+    const defaultDark = figma.root.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
 
-    console.log(postComponentSet);
-    console.log(postComponentSet.children);
-    console.log(postComponentSet.name);
+    if (pluginMessage.darkModeState){
+        defaultDark.createInstance();
+    } else {
+        defaultVariant.createInstance();
+    }
 
-    console.log(pluginMessage.name);
-    console.log(pluginMessage.username);
-    console.log(pluginMessage.description);
-    console.log(pluginMessage.darkModeState);
-    console.log(pluginMessage.imageVariant);
 };
