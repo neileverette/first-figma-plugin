5542f691ce004a54eb091f393d03b288accddb9c
Neil Everette <neil.everette@gmail.com>
2024-07-21 10:40:40 -0500

    Implemented workink switch statement to toggle different variants

--- code.ts
+++ code.ts
@@ -7,7 +7,7 @@ figma.loadAllPagesAsync();
 figma.ui.onmessage = pluginMessage => {
     const postComponentSet = figma.root.findOne(node => node.type == "COMPONENT_SET" && node.name == "post") as ComponentSetNode;
     const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
-    const defaultDark = figma.root.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
+    //const defaultDark = figma.root.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
 
     let selectedVariant;
     selectedVariant = "hello world";
@@ -18,27 +18,35 @@ figma.ui.onmessage = pluginMessage => {
         switch(pluginMessage.imageVariant) {
             case "2":
                 //create instance of dark mode, single image
+                selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=single, Dark mode=true") as ComponentNode;
+                selectedVariant.createInstance();
                 break;
             case "3":
                 //create instance of dark mode, carousel
+                selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=carousel, Dark mode=true") as ComponentNode;
+                selectedVariant.createInstance();
                 break;
             default:
-                //defaultDark.createInstance();
-                //console.log(selectedVariant)
-                //create instance of dark mode, no image
-                selectedVariant = postComponentSet.defaultVariant as ComponentNode;
+                // create instance of dark mode, no image
+                selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true") as ComponentNode;
+                selectedVariant.createInstance();
                 break;
         } 
     } else {
         switch(pluginMessage.imageVariant) {
             case "2":
                 //create instance of light mode, single image
+                selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=single, Dark mode=false") as ComponentNode;
+                selectedVariant.createInstance();
                 break;
             case "3":
                 //create instance of light mode, carousel
+                selectedVariant = postComponentSet.findOne(node => node.type == "COMPONENT" && node.name == "Image=carousel, Dark mode=false") as ComponentNode;
+                selectedVariant.createInstance();
                 break;
             default:
-                selectedVariant = postComponentSet.defaultVariant as ComponentNode;    
+                selectedVariant = postComponentSet.defaultVariant as ComponentNode;
+                selectedVariant.createInstance();    
                 //defaultVariant.createInstance();
                 //create instance of light mode, no image
                 break;
